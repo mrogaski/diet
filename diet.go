2be0fd354b70d165108019ec8ae226380e012ebc
Mark Rogaski <mrogaski@pobox.com>
2022-10-01 12:06:13 -0400

    Removed original Insert and added Contains.

--- diet.go
+++ diet.go
@@ -1,14 +1,16 @@
 package diet
 
+import "fmt"
+
 type Tree[T Integer] struct {
-	Root *Node[T]
+	Interval *Interval[T]
+	Left     *Tree[T]
+	Right    *Tree[T]
 }
 
-type Node[T Integer] struct {
+type Interval[T Integer] struct {
 	First T
 	Last  T
-	Left  *Node[T]
-	Right *Node[T]
 }
 
 // NewTree returns a pointer to a new discrete interval encoding tree.
@@ -16,36 +18,13 @@ func NewTree[T Integer]() *Tree[T] {
 	return &Tree[T]{}
 }
 
-func (tree *Tree[T]) Insert(elem T) error {
-	tree.Root = child(tree.Root, elem)
-	node := tree.Root
-
-	for {
-		switch {
-		case elem == node.First-1:
-			node.First = elem
-
-			return nil
-		case elem < node.First:
-			node.Left = child(node.Left, elem)
-			node = node.Left
-		case elem == node.Last+1:
-			node.Last = elem
-
-			return nil
-		case elem > node.Last:
-			node.Right = child(node.Right, elem)
-			node = node.Right
-		default:
-			return nil
-		}
+func (tree *Tree[T]) Contains(elem T) bool {
+	if tree.Interval == nil {
+		return false
 	}
+	return elem >= tree.Interval.First && elem <= tree.Interval.Last
 }
 
-func child[T Integer](node *Node[T], elem T) *Node[T] {
-	if node != nil {
-		return node
-	}
-
-	return &Node[T]{First: elem, Last: elem}
+func (tree *Tree[T]) Insert(elem T) error {
+	return fmt.Errorf("not implemented")
 }
--- diet_test.go
+++ diet_test.go
@@ -16,7 +16,79 @@ func TestNewTree(t *testing.T) {
 	assert.IsType(t, &diet.Tree[int]{}, tree)
 }
 
+func TestTree_Contains(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		tree     *diet.Tree[int]
+		elem     int
+		expected bool
+	}{
+		{name: "empty", tree: &diet.Tree[int]{}, elem: 0, expected: false},
+		{
+			name:     "single element",
+			tree:     &diet.Tree[int]{Interval: &diet.Interval[int]{First: 0, Last: 0}},
+			elem:     0,
+			expected: true,
+		},
+		{
+			name:     "single node, internal",
+			tree:     &diet.Tree[int]{Interval: &diet.Interval[int]{First: -2, Last: 2}},
+			elem:     0,
+			expected: true,
+		},
+		{
+			name:     "single node, left edge",
+			tree:     &diet.Tree[int]{Interval: &diet.Interval[int]{First: 0, Last: 2}},
+			elem:     0,
+			expected: true,
+		},
+		{
+			name:     "single node, right edge",
+			tree:     &diet.Tree[int]{Interval: &diet.Interval[int]{First: -2, Last: 0}},
+			elem:     0,
+			expected: true,
+		},
+		{
+			name:     "single node, predecessor",
+			tree:     &diet.Tree[int]{Interval: &diet.Interval[int]{First: -2, Last: 2}},
+			elem:     -3,
+			expected: false,
+		},
+		{
+			name:     "single node, successor",
+			tree:     &diet.Tree[int]{Interval: &diet.Interval[int]{First: -2, Last: 2}},
+			elem:     3,
+			expected: false,
+		},
+		{
+			name:     "single node, less than",
+			tree:     &diet.Tree[int]{Interval: &diet.Interval[int]{First: -2, Last: 2}},
+			elem:     -4,
+			expected: false,
+		},
+		{
+			name:     "single node, greater than",
+			tree:     &diet.Tree[int]{Interval: &diet.Interval[int]{First: -2, Last: 2}},
+			elem:     4,
+			expected: false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			assert.Equal(t, tt.expected, tt.tree.Contains(tt.elem))
+		})
+	}
+}
+
 func TestTree_Insert(t *testing.T) {
+	t.Skip("not implemented")
 	t.Parallel()
 
 	tests := []struct {
@@ -26,38 +98,60 @@ func TestTree_Insert(t *testing.T) {
 		expected *diet.Tree[int]
 	}{
 		{
-			name:     "initial",
-			tree:     &diet.Tree[int]{},
-			input:    []int{0},
-			expected: &diet.Tree[int]{Root: &diet.Node[int]{First: 0, Last: 0}},
+			name:  "initial",
+			tree:  &diet.Tree[int]{},
+			input: []int{0},
+			expected: &diet.Tree[int]{
+				Interval: &diet.Interval[int]{
+					First: 0,
+					Last:  0,
+				},
+			},
 		},
 		{
-			name:     "duplicate",
-			tree:     &diet.Tree[int]{},
-			input:    []int{0, 0},
-			expected: &diet.Tree[int]{Root: &diet.Node[int]{First: 0, Last: 0}},
+			name:  "duplicate",
+			tree:  &diet.Tree[int]{},
+			input: []int{0, 0},
+			expected: &diet.Tree[int]{
+				Interval: &diet.Interval[int]{
+					First: 0,
+					Last:  0,
+				},
+			},
 		},
 		{
-			name:     "predecessor",
-			tree:     &diet.Tree[int]{},
-			input:    []int{0, -1},
-			expected: &diet.Tree[int]{Root: &diet.Node[int]{First: -1, Last: 0}},
+			name:  "predecessor",
+			tree:  &diet.Tree[int]{},
+			input: []int{0, -1},
+			expected: &diet.Tree[int]{
+				Interval: &diet.Interval[int]{
+					First: -1,
+					Last:  0,
+				},
+			},
 		},
 		{
-			name:     "successor",
-			tree:     &diet.Tree[int]{},
-			input:    []int{0, 1},
-			expected: &diet.Tree[int]{Root: &diet.Node[int]{First: 0, Last: 1}},
+			name:  "successor",
+			tree:  &diet.Tree[int]{},
+			input: []int{0, 1},
+			expected: &diet.Tree[int]{
+				Interval: &diet.Interval[int]{
+					First: 0,
+					Last:  1,
+				},
+			},
 		},
 		{
 			name:  "left",
 			tree:  &diet.Tree[int]{},
 			input: []int{0, -5},
 			expected: &diet.Tree[int]{
-				Root: &diet.Node[int]{
+				Interval: &diet.Interval[int]{
 					First: 0,
 					Last:  0,
-					Left: &diet.Node[int]{
+				},
+				Left: &diet.Tree[int]{
+					Interval: &diet.Interval[int]{
 						First: -5,
 						Last:  -5,
 					},
@@ -69,10 +163,12 @@ func TestTree_Insert(t *testing.T) {
 			tree:  &diet.Tree[int]{},
 			input: []int{0, 5},
 			expected: &diet.Tree[int]{
-				Root: &diet.Node[int]{
+				Interval: &diet.Interval[int]{
 					First: 0,
 					Last:  0,
-					Right: &diet.Node[int]{
+				},
+				Right: &diet.Tree[int]{
+					Interval: &diet.Interval[int]{
 						First: 5,
 						Last:  5,
 					},
