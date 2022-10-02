e8028ef818ead19bf2c8814a79de8a9b5b7d669a
Mark Rogaski <mrogaski@pobox.com>
2022-10-02 01:43:07 -0400

    Split out Interval (#2)
    * Moved Interval implementation to a separate source file.
    
    * Removed default cases for tree search loops.

--- diet.go
+++ diet.go
@@ -43,8 +43,6 @@ func (tree *Tree[T]) Contains(elem T) bool {
 			}
 
 			t = t.Right
-		default:
-			return false
 		}
 	}
 }
@@ -79,8 +77,6 @@ func (tree *Tree[T]) Insert(elem T) {
 		case elem > t.Interval.Last:
 			t.Right = ternary(t.Right == nil, &Tree[T]{}, t.Right)
 			t = t.Right
-		default:
-			return
 		}
 	}
 }
@@ -138,42 +134,5 @@ func (tree *Tree[T]) joinRight() {
 		} else {
 			parent.Left = nil
 		}
-	}
-}
-
-// Interval represents a subset consisting of a sequence of contiguous elements.
-// Single-element subsets are represented by an interval where First and Last are equal.
-type Interval[T Integer] struct {
-	First T
-	Last  T
-}
-
-func (i *Interval[T]) has(elem T) bool {
-	return elem >= i.First && elem <= i.Last
-}
-
-func (i *Interval[T]) adjacent(other *Interval[T]) bool {
-	return i.adjacentElement(other.First) || i.adjacentElement(other.Last)
-}
-
-func (i *Interval[T]) adjacentElement(elem T) bool {
-	return i.leftAdjacentElement(elem) || i.rightAdjacentElement(elem)
-}
-
-func (i *Interval[T]) leftAdjacentElement(elem T) bool {
-	return elem == i.First-1
-}
-
-func (i *Interval[T]) rightAdjacentElement(elem T) bool {
-	return elem == i.Last+1
-}
-
-func (i *Interval[T]) merge(other *Interval[T]) {
-	if i.First > other.First {
-		i.First = other.First
-	}
-
-	if i.Last < other.Last {
-		i.Last = other.Last
 	}
 }
